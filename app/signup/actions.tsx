bc9eb7ca1fcdd468e1346a564e418680033515f0
shekhar <mauryashekhar9837@gmail.com>
2025-04-14 10:39:19 +0530

    fix: update redirect paths in login and signup actions to point to root

--- app/signup/actions.tsx
+++ app/signup/actions.tsx
@@ -22,7 +22,7 @@ export async function login(formData: FormData) {
   }
 
   revalidatePath('/', 'layout')
-  redirect('/dashboard')
+  redirect('/')
 }
 
 export async function signup(formData: FormData) {
@@ -49,5 +49,5 @@ export async function signup(formData: FormData) {
   }
 
   revalidatePath('/', 'layout')
-  redirect('/dashboard')
+  redirect('/')
 }
--- middleware.ts
+++ middleware.ts
@@ -27,5 +27,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/login', '/signup']
+  matcher: ['/:path*', '/login', '/signup']
 }
