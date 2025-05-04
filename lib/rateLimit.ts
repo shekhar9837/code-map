cb5620d12596256193a30ca798205eab97f5268c
shekhar <mauryashekhar9837@gmail.com>
2025-05-04 20:12:46 +0530

    incresed rate limit

--- lib/rateLimit.ts
+++ lib/rateLimit.ts
@@ -3,13 +3,13 @@ import { Redis } from "@upstash/redis";
 
 export const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-   limiter: Ratelimit.slidingWindow(5, "10 h"), analytics: true,
+   limiter: Ratelimit.slidingWindow(10, "10 s"), analytics: true,
   prefix: "@upstash/ratelimit",
 });
 
 export const authRateLimit = new Ratelimit({
   redis: Redis.fromEnv(),
-   limiter: Ratelimit.slidingWindow(5, "1 m"), analytics: true,
+   limiter: Ratelimit.slidingWindow(30, "1 m"), analytics: true,
   prefix: "@upstash/ratelimit",
 });
 
